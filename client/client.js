fcfd8fe3b30837220756d187c52bba2f1ff33327
patricksilva <pfivel@gmail.com>
2023-03-12 19:34:55 -0600

    Another progress check point

--- client/client.js
+++ client/client.js
@@ -17,13 +17,47 @@ const onChatSubmitted = (sock) => (e) => {
     sock.emit('message', text);
 };
 
+const getBoard = (canvas) => {
+
+    const ctx = canvas.getContext('2d');
+
+    const fillCircle = (x, y, color) => {
+        ctx.fillStyle = color;
+        ctx.fillRect(x - 10, y - 10, 20, 20);
+
+    };
+
+    return { fillCircle };
+
+};
+
+const getClickCoordinates = (element, ev) => {
+    const { top, left } = element.getBoundingClientRect();
+    const { clientX, clientY } = ev;
+
+    return {
+        x: clientX - left,
+        y: clientY - top
+    };
+};
+
 (() => {
-   
+
+    const canvas = document.querySelector('canvas');
+    const { fillCircle } = getBoard(canvas);
     const sock = io();
 
+    const onClick = (e) => {
+        const { x, y } = getClickCoordinates(canvas, e);
+        sock.emit('turn', { x, y });
+    };
+
     sock.on('message', log);
+    sock.on('turn', ({ x, y }) => fillCircle(x,y));
 
     document
     .querySelector('#chat-form')
     .addEventListener('submit', onChatSubmitted(sock));
+
+    canvas.addEventListener('click', onClick);
 })();
